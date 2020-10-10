665b026895099bbaea024ef3ddee2468db61d284
肖振男 <190657@lifeat.cn>
2020-10-10 18:03:55 +0800

    工程打包

---
+++ support-etl-framework/etl-plugins/mysql-plugin/src/main/java/com/weiwan/support/etl/plugins/MysqlReader.java
@@ -0,0 +1,15 @@
+package com.weiwan.support.etl.plugins;
+
+/**
+ * @Author: xiaozhennan
+ * @Date: 2020/10/10 14:19
+ * @Package: PACKAGE_NAME.com.weiwan.support.etl.plugins.MysqlReader
+ * @ClassName: com.weiwan.support.etl.plugins.MysqlReader
+ * @Description:
+ **/
+public class MysqlReader {
+
+    public static void main(String[] args) {
+        System.out.println("mysql reader");
+    }
+}
