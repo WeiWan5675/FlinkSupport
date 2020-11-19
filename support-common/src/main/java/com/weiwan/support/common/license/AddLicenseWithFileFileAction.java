a6c4693f4c75a8827a4da0d30c400bf2bac3b85e
肖振男 <190657@lifeat.cn>
2020-11-19 15:27:58 +0800

    add license to source file head

--- support-common/src/main/java/com/weiwan/support/common/license/AddLicenseWithFileFileAction.java
+++ support-common/src/main/java/com/weiwan/support/common/license/AddLicenseWithFileFileAction.java
@@ -1,11 +1,11 @@
-/**
- * Copyright 2019 蒋文忠
+/*
+ *      Copyright [2020] [xiaozhennan1995@gmail.com]
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
--- support-common/src/main/java/com/weiwan/support/common/license/LicenseForJavaFileUtils.java
+++ support-common/src/main/java/com/weiwan/support/common/license/LicenseForJavaFileUtils.java
@@ -1,11 +1,11 @@
-/**
- * Copyright 2019 蒋文忠
+/*
+ *      Copyright [2020] [xiaozhennan1995@gmail.com]
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,20 +23,20 @@ import java.io.File;
  */
 public class LicenseForJavaFileUtils {
 
-    public static final String FILE_SUFFIX = "java";
+    public static final String FILE_SUFFIX = "";
 
     public static void main(String[] args) {
 
-        String javaFilesDir = "/Users/jiangwenzhong/android/workspace/github/JLicenseForJavaFileUtil/app/src/main/java/com/jwz/jlicenseforjavafileutil/";
+        String javaFilesDir = "F:\\Project\\FlinkSupport";
 
-        String licenseStr = "/**\n" +
-                " * Copyright 2019 蒋文忠\n" +
+        String licenseStr = "/*\n" +
+                " *      Copyright [2020] [xiaozhennan1995@gmail.com]\n" +
                 " * <p>\n" +
                 " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
                 " * you may not use this file except in compliance with the License.\n" +
                 " * You may obtain a copy of the License at\n" +
                 " * <p>\n" +
-                " * http://www.apache.org/licenses/LICENSE-2.0\n" +
+                " *      http://www.apache.org/licenses/LICENSE-2.0\n" +
                 " * <p>\n" +
                 " * Unless required by applicable law or agreed to in writing, software\n" +
                 " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
