afac051077978bf27fda8e0ed3b1c8790652d6cf
肖振男 <190657@lifeat.cn>
2020-12-03 16:43:53 +0800

    提交

---
+++ bin/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+source /etc/profile
+source "$(cd "`dirname "$0"`"/..; pwd)"/bin/support-env
+MAVEN_HOME=""
+
+$MAVEN_HOME/bin/mvn clean package install -Dmaven.test.skip=true -f $1
+
+
+exit $?
